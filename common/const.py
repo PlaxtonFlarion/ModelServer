13b587ca3d83d8d3de21bc288963c428ad78578d
AceKeppel <AceKeppel@outlook.com>
2025-10-25 01:42:34 +0800

    bug fixes

--- common/const.py
+++ common/const.py
@@ -5,9 +5,10 @@
 #   \____\___/|_| |_|___/\__|
 #
 
-SHOW_LEVEL         = r"INFO"
-PRINT_FORMAT       = r"<bold><level>{level}</level></bold>: <bold><cyan>{message}</cyan></bold>"
-WRITE_FORMAT       = r"{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}"
+# ---- Notes: 格式 ----
+SHOW_LEVEL   = r"INFO"
+PRINT_FORMAT = r"<bold><level>{level}</level></bold>: <bold><cyan>{message}</cyan></bold>"
+WRITE_FORMAT = r"{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}"
 
 
 if __name__ == '__main__':
