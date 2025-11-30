2d518926e8f991f4454f83bd8c38eb73fe2009dc
AceKeppel <AceKeppel@outlook.com>
2025-11-30 09:08:12 +0800

    bug fixes

---
+++ middlewares/exception.py
@@ -0,0 +1,48 @@
+import json
+import modal
+from functools import wraps
+from loguru import logger
+from fastapi.responses import JSONResponse
+from starlette.requests import ClientDisconnect
+
+
+def with_exception_handling(func):
+    """异常中间件"""
+
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except ClientDisconnect as e:
+            logger.error(e)
+            return JSONResponse(
+                content={
+                    "Error": "Client disconnected during upload"
+                },
+                status_code=499
+            )
+        except json.JSONDecodeError as e:
+            logger.error(e)
+            return JSONResponse(
+                content={
+                    "Error": "Invalid JSON payload"
+                },
+                status_code=400
+            )
+        except modal.exception.InvalidError as e:
+            logger.error(e)
+            return JSONResponse(
+                content={
+                    "Error": f"Modal error: {str(e)}"
+                },
+                status_code=500
+            )
+        except Exception as e:
+            logger.error(e)
+            return JSONResponse(
+                content={
+                    "Error": f"Unexpected error: {str(e)}"
+                },
+                status_code=500
+            )
+    return wrapper
